254a84c080dbb8dbdaa6b623c9806a3efd320dac
itdocuments <hoangvanlam50@gmail.com>
2021-08-22 19:39:22 +0700

    code tv

--- phpProject/admin/includes/sidebar.php
+++ phpProject/admin/includes/sidebar.php
@@ -1,71 +1,78 @@
      <aside id="slide-out" class="side-nav white fixed">
-                <div class="side-nav-wrapper">
-                    <div class="sidebar-profile">
-                        <div class="sidebar-profile-image">
-                            <img src="../assets/images/profile-image.png" class="circle" alt="">
-                        </div>
-                        <div class="sidebar-profile-info">
-                       
-                                <p>Admin</p>
+         <div class="side-nav-wrapper">
+             <div class="sidebar-profile">
+                 <div class="sidebar-profile-image">
+                     <img src="../assets/images/profile-image.png" class="circle" alt="">
+                 </div>
+                 <div class="sidebar-profile-info">
 
-                         
-                        </div>
-                    </div>
-            
-                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
-                    <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php"><i class="material-icons">settings_input_svideo</i>Dashboard</a></li>
-                    <li class="no-padding">
-                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">apps</i>Department<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
-                        <div class="collapsible-body">
-                            <ul>
-                                <li><a href="adddepartment.php">Add Department</a></li>
-                                <li><a href="managedepartments.php">Manage Department</a></li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="no-padding">
-                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">code</i>Leave Type<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
-                        <div class="collapsible-body">
-                            <ul>
-                                <li><a href="addleavetype.php">Add Leave Type</a></li>
-                                <li><a href="manageleavetype.php">Manage Leave Type</a></li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="no-padding">
-                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">account_box</i>Employees<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
-                        <div class="collapsible-body">
-                            <ul>
-                                <li><a href="addemployee.php">Add Employee</a></li>
-                                <li><a href="manageemployee.php">Manage Employee</a></li>
-       
-                            </ul>
-                        </div>
-                    </li>
-
-   <li class="no-padding">
-                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">desktop_windows</i>Leave Management<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
-                        <div class="collapsible-body">
-                            <ul>
-                                <li><a href="leaves.php">All Leaves </a></li>
-                                <li><a href="pending-leavehistory.php">Pending Leaves </a></li>
-                                <li><a href="approvedleave-history.php">Approved Leaves</a></li>
-                                  <li><a href="notapproved-leaves.php">Not Approved Leaves</a></li>
-       
-                            </ul>
-                        </div>
-                    </li>
+                     <p>Admin</p>
 
 
+                 </div>
+             </div>
+
+             <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+                 <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php"><i
+                             class="material-icons">settings_input_svideo</i>Dashboard</a></li>
+                 <li class="no-padding">
+                     <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">apps</i>Bộ phận<i
+                             class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                     <div class="collapsible-body">
+                         <ul>
+                             <li><a href="managedepartments.php">Quản lý bộ phận</a></li>
+                             <li><a href="adddepartment.php">Thêm bộ phận</a></li>
+                         </ul>
+                     </div>
+                 </li>
+                 <li class="no-padding">
+                     <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">code</i>Nghỉ Phép<i
+                             class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                     <div class="collapsible-body">
+                         <ul>
+                             <li><a href="addleavetype.php">Thêm loại phép</a></li>
+                             <li><a href="manageleavetype.php">Quản lý loại phép</a></li>
+                         </ul>
+                     </div>
+                 </li>
+                 <li class="no-padding">
+                     <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">account_box</i>Nhân
+                         Viên<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                     <div class="collapsible-body">
+                         <ul>
+                             <li><a href="addemployee.php">Thêm nhân viên</a></li>
+                             <li><a href="manageemployee.php">Quản lý nhân viên</a></li>
+
+                         </ul>
+                     </div>
+                 </li>
+
+                 <li class="no-padding">
+                     <a class="collapsible-header waves-effect waves-grey"><i
+                             class="material-icons">desktop_windows</i>Quản lí nghỉ phép<i
+                             class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                     <div class="collapsible-body">
+                         <ul>
+                             <li><a href="leaves.php">Tất cả giấy nghỉ phép</a></li>
+                             <li><a href="pending-leavehistory.php">đang chờ</a></li>
+                             <li><a href="approvedleave-history.php">Phép được duyệt</a></li>
+                             <li><a href="notapproved-leaves.php">Phép không được duyệt</a></li>
+
+                         </ul>
+                     </div>
+                 </li>
 
 
-                        <li class="no-padding">
-                                <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Sign Out</a>
-                            </li>  
-                 
 
-                 
-              
-                </ul> 
-                </div>
-            </aside>
+
+                 <li class="no-padding">
+                     <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Sign
+                         Out</a>
+                 </li>
+
+
+
+
+             </ul>
+         </div>
+     </aside>
